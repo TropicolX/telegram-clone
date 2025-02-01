e9bb055fc2b24af885b84da423c8c33547cad2db
TropicolX <jacobsbusayo@gmail.com>
2025-02-01 15:57:01 +0100

    remove pageloading component as a whole

--- src/app/a/layout.tsx
+++ src/app/a/layout.tsx
@@ -7,7 +7,6 @@ import { Chat } from 'stream-chat-react';
 import { StreamVideo, StreamVideoClient } from '@stream-io/video-react-sdk';
 import clsx from 'clsx';
 
-import PageLoading from '@/components/PageLoading';
 import Sidebar from '@/components/Sidebar';
 
 interface LayoutProps {
@@ -81,7 +80,20 @@ export default function Layout({ children }: LayoutProps) {
     if (user) setUpChatAndVideo();
   }, [user, videoClient, chatClient]);
 
-  if (loading) return <PageLoading sidebarWidth={sidebarWidth} />;
+  if (loading)
+    return (
+      <div className="flex h-full w-full">
+        <div
+          style={{
+            width: `${sidebarWidth || defaultWidth}px`,
+          }}
+          className="bg-background h-full flex-shrink-0 relative"
+        ></div>
+        <div className="relative flex flex-col items-center w-full h-full overflow-hidden border-l border-solid border-l-color-borders">
+          <div className="chat-background absolute top-0 left-0 w-full h-full -z-10 overflow-hidden bg-theme-background"></div>
+        </div>
+      </div>
+    );
 
   return (
     <Chat client={chatClient!}>
