5e0855cc3c773340597b9f56a3343adf9c05f7fb
TropicolX <jacobsbusayo@gmail.com>
2025-02-01 14:08:26 +0100

    update lib.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -17,3 +17,41 @@ export const getLastSeen = (lastActive: string) => {
     return 'last seen a long time ago';
   }
 };
+
+export const getOrdinalSuffix = (day: number) => {
+  if (day > 3 && day < 21) return 'th';
+  switch (day % 10) {
+    case 1:
+      return 'st';
+    case 2:
+      return 'nd';
+    case 3:
+      return 'rd';
+    default:
+      return 'th';
+  }
+};
+
+export const formatDate = (date: Date) => {
+  const now = new Date();
+  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  const yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+
+  const isToday = date >= today;
+  const isYesterday = date >= yesterday && date < today;
+  const isWithinWeek =
+    date >= new Date(today.getTime() - 6 * 24 * 60 * 60 * 1000);
+
+  if (isToday) {
+    return 'Today';
+  } else if (isYesterday) {
+    return 'Yesterday';
+  } else if (isWithinWeek) {
+    return date.toLocaleDateString('en-US', { weekday: 'long' });
+  } else {
+    return `${date.toLocaleDateString('en-US', {
+      month: 'short',
+    })} ${date.getDate()}${getOrdinalSuffix(date.getDate())}`;
+  }
+};
